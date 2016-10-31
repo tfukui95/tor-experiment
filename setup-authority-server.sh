33569f4f3171240c403ff146e7f32d125aeff32a
tfukui95 <tef243@nyu.edu>
2016-10-31 13:58:43 -0400

    Update setup-authority-server.sh

--- setup-authority-server.sh
+++ setup-authority-server.sh
@@ -16,5 +16,46 @@ sudo tor-gencert --create-identity-key -m 12 -a 192.168.1.4:7000 \
             -s /var/lib/tor/keys/authority_signing_key \
             -c /var/lib/tor/keys/authority_certificate 
             
-sudo tor --list-fingerprint --orport 1 \
+tor --list-fingerprint --orport 1 \
     --dirserver "x 127.0.0.1:1 ffffffffffffffffffffffffffffffffffffffff" 
+    
+finger1=$(sudo cat /var/lib/tor/keys/authority_certificate  | grep fingerprint | cut -f 2 -d ' ')
+finger2=$(sudo cat /var/lib/tor/fingerprint | cut -f 2 -d ' ')
+
+# http://stackoverflow.com/questions/7875540/how-do-you-write-multiple-line-configuration-file-using-bash-and-use-variables
+# http://stackoverflow.com/questions/18836853/sudo-cat-eof-file-doesnt-work-sudo-su-does
+sudo bash -c "cat >/etc/tor/torrc <<EOL
+TestingTorNetwork 1
+DataDirectory /var/lib/tor
+RunAsDaemon 1
+ConnLimit 60
+Nickname RS4
+ShutdownWaitLength 0
+PidFile /var/lib/tor/pid
+Log notice file /var/lib/tor/notice.log
+Log info file /var/lib/tor/info.log
+ProtocolWarnings 1
+SafeLogging 0
+DisableDebuggerAttachment 0
+DirAuthority RS4 orport=5000 no-v2 hs v3ident=$finger1 192.168.1.4:7000 $finger2
+
+SocksPort 0
+OrPort 5000
+Address 192.168.1.4
+DirPort 7000
+
+# An exit policy that allows exiting to IPv4 LAN
+ExitPolicy accept 192.168.1.0/24:*
+
+# An exit policy that allows exiting to IPv6 localhost
+ExitPolicy accept [::1]:*
+IPv6Exit 1
+
+AuthoritativeDirectory 1
+V3AuthoritativeDirectory 1
+ContactInfo auth0@test.test
+ExitPolicy reject *:*
+TestingV3AuthInitialVotingInterval 300
+TestingV3AuthInitialVoteDelay 20
+TestingV3AuthInitialDistDelay 20
+EOL"
