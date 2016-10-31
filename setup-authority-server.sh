7dc350079447929ab5ce0564c627d5953822a7ac
tfukui95 <tef243@nyu.edu>
2016-10-31 13:34:22 -0400

    Update setup-authority-server.sh

--- setup-authority-server.sh
+++ setup-authority-server.sh
@@ -1,6 +1,7 @@
 sudo sh -c 'echo "deb http://deb.torproject.org/torproject.org trusty main" >> /etc/apt/sources.list'
 sudo sh -c 'echo "deb-src http://deb.torproject.org/torproject.org trusty main" >> /etc/apt/sources.list'
 
+# TODO - one of these commands don't seem to run when using script
 sudo gpg --keyserver keys.gnupg.net --recv 886DDD89
 sudo gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | sudo apt-key add -
 
@@ -8,11 +9,12 @@ sudo apt-get update
 sudo apt-get -y install tor deb.torproject.org-keyring vim
 
 sudo mkdir /var/lib/tor/keys
+# TODO - this requires an interactive password prompt. Is there any way 
+# to automate it?
 sudo tor-gencert --create-identity-key -m 12 -a 192.168.1.4:7000 \
             -i /var/lib/tor/keys/authority_identity_key \
             -s /var/lib/tor/keys/authority_signing_key \
             -c /var/lib/tor/keys/authority_certificate 
             
 sudo tor --list-fingerprint --orport 1 \
-    --dirserver "x 127.0.0.1:1 ffffffffffffffffffffffffffffffffffffffff" \
-    --datadirectory /var/lib/tor/
+    --dirserver "x 127.0.0.1:1 ffffffffffffffffffffffffffffffffffffffff" 
