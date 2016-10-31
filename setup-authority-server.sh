8682dcdf8064dc944a61d1b57d82989279bc8682
tfukui95 <tef243@nyu.edu>
2016-10-31 13:26:52 -0400

    Update setup-authority-server.sh

--- setup-authority-server.sh
+++ setup-authority-server.sh
@@ -1,3 +1,12 @@
+sudo sh -c 'echo "deb http://deb.torproject.org/torproject.org trusty main" >> /etc/apt/sources.list'
+sudo sh -c 'echo "deb-src http://deb.torproject.org/torproject.org trusty main" >> /etc/apt/sources.list'
+
+sudo gpg — keyserver keys.gnupg.net — recv 886DDD89
+sudo gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | sudo apt-key add -
+
+sudo apt-get update
+sudo apt-get -y install tor deb.torproject.org-keyring vim
+
 sudo mkdir /var/lib/tor/keys
 sudo tor-gencert --create-identity-key -m 12 -a 192.168.1.4:7000 \
             -i /var/lib/tor/keys/authority_identity_key \
