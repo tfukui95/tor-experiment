fbdbb179dbe32b8fc5c9842b60d222719e5435d5
tfukui95 <tef243@nyu.edu>
2016-10-31 13:28:22 -0400

    Update setup-authority-server.sh

--- setup-authority-server.sh
+++ setup-authority-server.sh
@@ -1,7 +1,7 @@
 sudo sh -c 'echo "deb http://deb.torproject.org/torproject.org trusty main" >> /etc/apt/sources.list'
 sudo sh -c 'echo "deb-src http://deb.torproject.org/torproject.org trusty main" >> /etc/apt/sources.list'
 
-sudo gpg — keyserver keys.gnupg.net — recv 886DDD89
+sudo gpg --keyserver keys.gnupg.net --recv 886DDD89
 sudo gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | sudo apt-key add -
 
 sudo apt-get update
