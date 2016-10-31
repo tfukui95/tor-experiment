7bde1082bfe417ced5e58804f88105f6aa5d564e
tfukui95 <tef243@nyu.edu>
2016-10-31 13:15:33 -0400

    Create setup-authority-server.sh

---
+++ setup-authority-server.sh
@@ -0,0 +1,9 @@
+sudo mkdir /var/lib/tor/keys
+sudo tor-gencert --create-identity-key -m 12 -a 192.168.1.4:7000 \
+            -i /var/lib/tor/keys/authority_identity_key \
+            -s /var/lib/tor/keys/authority_signing_key \
+            -c /var/lib/tor/keys/authority_certificate 
+            
+sudo tor --list-fingerprint --orport 1 \
+    --dirserver "x 127.0.0.1:1 ffffffffffffffffffffffffffffffffffffffff" \
+    --datadirectory /var/lib/tor/
