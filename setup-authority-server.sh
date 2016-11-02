2ba5c66ca119e0eb45670632a191b5c6c40a1977
tfukui95 <tef243@nyu.edu>
2016-11-02 12:34:25 -0400

    Update setup-authority-server.sh

--- setup-authority-server.sh
+++ setup-authority-server.sh
@@ -6,7 +6,7 @@ sudo gpg --keyserver keys.gnupg.net --recv 886DDD89
 sudo gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | sudo apt-key add -
 
 sudo apt-get update
-sudo apt-get -y install tor deb.torproject.org-keyring vim apache2
+sudo apt-get -y install tor deb.torproject.org-keyring vim apache2 curl
 
 sudo mkdir /var/lib/tor/keys
 # TODO - this requires an interactive password prompt. Is there any way 
