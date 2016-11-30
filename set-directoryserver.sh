d008a50a757ad437ea345f9104769978e8b28de2
tfukui95 <tef243@nyu.edu>
2016-11-30 12:48:24 -0500

    Create set-directoryserver.sh

---
+++ set-directoryserver.sh
@@ -0,0 +1,110 @@
+sudo sh -c 'echo "deb http://deb.torproject.org/torproject.org trusty main" >> /etc/apt/sources.list'
+sudo sh -c 'echo "deb-src http://deb.torproject.org/torproject.org trusty main" >> /etc/apt/sources.list'
+
+sudo gpg --keyserver keys.gnupg.net --recv 886DDD89
+sudo gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | sudo apt-key add -
+
+sudo apt-get update
+sudo apt-get -y install tor deb.torproject.org-keyring vim curl tor-arm
+
+sudo /etc/init.d/tor stop
+
+sudo -u debian-tor mkdir /var/lib/tor/keys
+sudo -u debian-tor tor-gencert --create-identity-key -m 12 -a 192.168.1.4:7000 \
+            -i /var/lib/tor/keys/authority_identity_key \
+            -s /var/lib/tor/keys/authority_signing_key \
+            -c /var/lib/tor/keys/authority_certificate
+
+sudo -u debian-tor tor --list-fingerprint --orport 1 \
+    --dirserver "x 127.0.0.1:1 ffffffffffffffffffffffffffffffffffffffff" \
+    --datadirectory /var/lib/tor/
+    
+finger1=$(sudo cat /var/lib/tor/keys/authority_certificate  | grep fingerprint | cut -f 2 -d ' ')
+finger2=$(sudo cat /var/lib/tor/fingerprint | cut -f 2 -d ' ')
+
+HOSTNAME=$(hostname -s)
+
+sudo bash -c "cat >/etc/tor/torrc <<EOL
+TestingTorNetwork 1
+DataDirectory /var/lib/tor
+RunAsDaemon 1
+ConnLimit 60
+Nickname $HOSTNAME
+ShutdownWaitLength 0
+PidFile /var/lib/tor/pid
+Log notice file /var/log/tor/notice.log
+Log info file /var/log/tor/info.log
+Log debug file /var/log/tor/debug.log
+ProtocolWarnings 1
+SafeLogging 0
+DisableDebuggerAttachment 0
+DirAuthority $HOSTNAME orport=5000 no-v2 hs v3ident=$finger1 192.168.1.4:7000 $finger2
+SocksPort 0
+OrPort 5000
+ControlPort 9051
+Address 192.168.1.4
+DirPort 7000
+# An exit policy that allows exiting to IPv4 LAN
+ExitPolicy accept 192.168.1.0/24:*
+
+AuthoritativeDirectory 1
+V3AuthoritativeDirectory 1
+ContactInfo auth0@test.test
+ExitPolicy reject *:*
+TestingV3AuthInitialVotingInterval 300
+TestingV3AuthInitialVoteDelay 20
+TestingV3AuthInitialDistDelay 20
+EOL"
+
+sudo apt-get update
+sudo apt-get -y install apache2 php5 libapache2-mod-php5
+
+sudo bash -c "cat >/var/www/html/router.conf <<EOL
+TestingTorNetwork 1
+DataDirectory /var/lib/tor
+RunAsDaemon 1
+ConnLimit 60
+ShutdownWaitLength 0
+PidFile /var/lib/tor/pid
+Log notice file /var/log/tor/notice.log
+Log info file /var/log/tor/info.log
+Log debug file /var/log/tor/debug.log
+ProtocolWarnings 1
+SafeLogging 0
+DisableDebuggerAttachment 0
+DirAuthority $HOSTNAME orport=5000 no-v2 hs v3ident=$finger1 192.168.1.4:7000 $finger2
+
+SocksPort 0
+OrPort 5000
+ControlPort 9051
+
+# An exit policy that allows exiting to IPv4 LAN
+ExitPolicy accept 192.168.1.0/24:*
+ExitPolicy accept 192.168.2.0/24:*
+ExitPolicy accept 192.168.3.0/24:*
+ExitPolicy accept 192.168.4.0/24:*
+EOL"
+
+sudo bash -c "cat >/var/www/html/client.conf <<EOL
+TestingTorNetwork 1
+DataDirectory /var/lib/tor
+RunAsDaemon 1
+ConnLimit 60
+ShutdownWaitLength 0
+PidFile /var/lib/tor/pid
+Log notice file /var/log/tor/notice.log
+Log info file /var/log/tor/info.log
+Log debug file /var/log/tor/debug.log
+ProtocolWarnings 1
+SafeLogging 0
+DisableDebuggerAttachment 0
+DirAuthority $HOSTNAME orport=5000 no-v2 hs v3ident=$finger1 192.168.1.4:7000 $finger2
+
+SocksPort 9050
+ControlPort 9051
+EOL"
+
+sudo /etc/init.d/tor start
+
+sudo cat /var/log/tor/debug.log | grep "Trusted"
+
