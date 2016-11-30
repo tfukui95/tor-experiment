24427feafcbe7bf857302db2ac5b36b0b2643491
tfukui95 <tef243@nyu.edu>
2016-11-30 12:53:04 -0500

    Create set-client.sh

---
+++ set-client.sh
@@ -0,0 +1,26 @@
+sudo sh -c 'echo "deb http://deb.torproject.org/torproject.org trusty main" >> /etc/apt/sources.list'
+sudo sh -c 'echo "deb-src http://deb.torproject.org/torproject.org trusty main" >> /etc/apt/sources.list'
+
+sudo gpg --keyserver keys.gnupg.net --recv 886DDD89
+sudo gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | sudo apt-key add -
+
+sudo apt-get update
+sudo apt-get -y install tor deb.torproject.org-keyring vim curl tor-arm
+
+sudo /etc/init.d/tor stop
+
+sudo -u debian-tor tor --list-fingerprint --orport 1 \
+    --dirserver "x 127.0.0.1:1 ffffffffffffffffffffffffffffffffffffffff" \
+    --datadirectory /var/lib/tor/
+
+sudo wget -O /etc/tor/torrc http://directoryserver/client.conf
+
+HOSTNAME=$(hostname -s)
+echo "Nickname $HOSTNAME" | sudo tee -a /etc/tor/torrc
+ADDRESS=$(hostname -I | tr " " "\n" | grep "192.168")
+for A in $ADDRESS; do
+  echo "Address $A" | sudo tee -a /etc/tor/torrc
+done
+
+sudo /etc/init.d/tor restart
+
