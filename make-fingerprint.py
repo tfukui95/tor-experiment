f3702949d823264f74b1cfbd531323a852a8cb1a
Takuya Fukui <tfukui95@gmail.com>
2017-03-21 15:11:15 -0400

    Added number markers

--- make-fingerprint.py
+++ make-fingerprint.py
@@ -5,13 +5,61 @@ import csv
 filename = 'finger.csv'
 with open(filename, 'rb') as csvfile:
   filereader = csv.reader(csvfile,delimiter= ',')
-  with open('finger2.csv', 'wb') as csvfile2:
+  with open('finger4.csv', 'wb') as csvfile2:
     filewriter= csv.writer(csvfile2, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    sizeMarker = 0
+    totalSizeP = 0
+    totalSizeN = 0
+    flag = 1
     for row in filereader:
       if (int(row[0]) > 85):
         if (row[2] == '192.168.5.200'):
-          filewriter.writerow(['+', row[0]])
+          if (flag > 0):
+            flag += 1
+            sizeMarker += int(row[0])
+            filewriter.writerow(['+', row[0]])
+          else:
+            if (flag == -1):
+              filewriter.writerow(['N', '1'])
+            elif (flag == -2):
+              filewriter.writerow(['N', '2'])
+            elif (flag <= -3 and flag >= -5):
+              filewriter.writerow(['N', '3-5'])
+            elif (flag <= -6 and flag >= -8):
+              filewriter.writerow(['N', '6-8'])
+            elif (flag <= -9 and flag >= -13):
+              filewriter.writerow(['N', '9-13'])
+            else:
+              filewriter.writerow(['N', '>14'])
+            filewriter.writerow(['S', sizeMarker])
+            sizeMarker = int(row[0])
+            flag = 1
+            filewriter.writerow(['+', row[0]])
+          totalSizeP += int(row[0])
         else:
-          filewriter.writerow(['-', row[0]])
+          if (flag < 0):
+            flag -= 1
+            sizeMarker += int(row[0])
+            filewriter.writerow(['-', row[0]])
+          else:
+            if (flag == 1):
+              filewriter.writerow(['N', '1'])
+            elif (flag == 2):
+              filewriter.writerow(['N', '2'])
+            elif (flag >= 3 and flag <= 5):
+              filewriter.writerow(['N', '3-5'])
+            elif (flag >= 6 and flag <= 8):
+              filewriter.writerow(['N', '6-8'])
+            elif (flag >= 9 and flag <= 13):
+              filewriter.writerow(['N', '9-13'])
+            else:
+              filewriter.writerow(['N', '>14'])
+            filewriter.writerow(['S', sizeMarker])
+            sizeMarker = int(row[0])
+            flag = -1
+            filewriter.writerow(['-', row[0]])
+          totalSizeN += int(row[0])
       else:
         print row[0]
+    filewriter.writerow(['T+',totalSizeP])
+    filewriter.writerow(['T-',totalSizeN])
