a95d200d2289d3bea8bb430f22b7abf3635f91be
tfukui95 <tef243@nyu.edu>
2017-03-30 12:28:16 -0400

    Update wfp-directoryserver.sh

--- wfp-directoryserver.sh
+++ wfp-directoryserver.sh
@@ -5,7 +5,7 @@ sudo gpg --keyserver keys.gnupg.net --recv 886DDD89
 sudo gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | sudo apt-key add -
 
 sudo apt-get update
-sudo apt-get -y install tor deb.torproject.org-keyring vim curl tor-arm 
+sudo apt-get -y install tor deb.torproject.org-keyring vim curl tor-arm tshark
 
 sudo /etc/init.d/tor stop
 
