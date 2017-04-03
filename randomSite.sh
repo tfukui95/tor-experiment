7a51f407e859bf0b72af1f74568ccd49a9ed78dc
tfukui95 <tef243@nyu.edu>
2017-04-03 13:10:12 -0400

    Update randomSite.sh

--- randomSite.sh
+++ randomSite.sh
@@ -3,10 +3,10 @@
 
 siteNumber=$(awk -v min=1 -v max=5 'BEGIN{srand(); print int(min+rand()*(max-mi n+1))}')
 
-if [ (echo $siteNumber) == 1 ] 
+if [ echo $siteNumber == 1 ] 
 then
   proxychains wget -p http://192.168.2.200/engineering.nyu.edu/
-elif [ (echo $siteNumber) == 2 ] 
+elif [ echo $siteNumber == 2 ] 
 then
   proxychains wget -p http://192.168.2.200/facebook.com/
 elif [ echo $siteNumber == 3 ]
