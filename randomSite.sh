ed939da9c27e7bd6e014c883e1b5a6fda37c60c1
tfukui95 <tef243@nyu.edu>
2017-04-03 12:58:56 -0400

    Update randomSite.sh

--- randomSite.sh
+++ randomSite.sh
@@ -3,15 +3,15 @@
 
 siteNumber=$(awk -v min=1 -v ma x=5 'BEGIN{srand(); print int(min+rand()*(max-mi n+1))}')
 
-if ["$siteNumber" == 1]; then
+if [ $siteNumber == 1 ]; then
   proxychains wget -p http://192.168.2.200/engineering.nyu.edu/
-elif ["$siteNumber" == 2]; then
+elif [ $siteNumber == 2 ]; then
   proxychains wget -p http://192.168.2.200/facebook.com/
-elif ["$siteNumber" == 3]; then
+elif [ $siteNumber == 3 ]; then
   proxychains wget -p http://192.168.2.200/youtube.com/
-elif ["$siteNumber" == 4]; then
+elif [ $siteNumber == 4 ]; then
   proxychains wget -p http://192.168.2.200/reddit.com/
-elif ["$siteNumber" == 5]; then
+elif [ $siteNumber == 5 ]; then
   proxychains wget -p http://192.168.2.200/www.mlb.com/mets
 fi
 
