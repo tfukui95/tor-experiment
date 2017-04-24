b799d1140335bd0e9cf2781f06349e8887d592c3
Fraida Fund <ffund@nyu.edu>
2017-04-24 19:39:35 -0400

    Add apt-get argument to fix issue that happens sometimes on some relays

--- wfp-client.sh
+++ wfp-client.sh
@@ -5,7 +5,7 @@ sudo gpg --keyserver keys.gnupg.net --recv 886DDD89
 sudo gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | sudo apt-key add -
 
 sudo apt-get update
-sudo apt-get -y install tor deb.torproject.org-keyring vim curl tor-arm python-pip
+sudo apt-get -y --force-yes install tor deb.torproject.org-keyring vim curl tor-arm python-pip
 sudo pip install stem
 
 wget https://raw.githubusercontent.com/tfukui95/tor-experiment/master/utilities/exit-relay.py
